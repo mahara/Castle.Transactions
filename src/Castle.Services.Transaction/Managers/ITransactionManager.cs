506f22b31382303cfb39fe04e07dfc26c923b486
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2021-05-30 23:19:22 +0700

    Refactored code (part b).

--- src/Castle.Services.Transaction/Managers/ITransactionManager.cs
+++ src/Castle.Services.Transaction/Managers/ITransactionManager.cs
@@ -19,64 +19,64 @@ using System;
 namespace Castle.Services.Transaction
 {
     /// <summary>
-    /// Manages the creation and disposal of <see cref="ITransaction"/> instances.
+    /// Manages the creation and disposal of <see cref="ITransaction" /> instances.
     /// </summary>
     public interface ITransactionManager : IEventPublisher
     {
         /// <summary>
-        /// Raised when a top level transaction was created
+        /// Dispose the transaction passed appropriately, removing it from the list of tracked transactions,
+        /// calling its dispose method and raise the <see cref="TransactionDisposed" /> event.
+        /// </summary>
+        /// <param name="transaction">The transaction to dispose.</param>
+        /// <exception cref="ArgumentNullException">If <paramref name="transaction" /> is <see langword="null" />.</exception>
+        void Dispose(ITransaction transaction);
+
+        /// <summary>
+        /// Raised when a top level transaction was created.
         /// </summary>
         event EventHandler<TransactionEventArgs> TransactionCreated;
 
         /// <summary>
-        /// Raised when a child transaction was created
-        /// </summary>
-        event EventHandler<TransactionEventArgs> ChildTransactionCreated;
-
-        /// <summary>
-        /// Raised when the transaction was disposed
+        /// Raised when the transaction was disposed.
         /// </summary>
         event EventHandler<TransactionEventArgs> TransactionDisposed;
 
         /// <summary>
-        /// <see cref="CreateTransaction(Castle.Services.Transaction.TransactionMode,Castle.Services.Transaction.IsolationMode,bool,bool)"/>.
+        /// Raised when a child transaction was created.
         /// </summary>
-        ITransaction CreateTransaction(TransactionMode transactionMode, IsolationMode isolationMode);
+        event EventHandler<TransactionEventArgs> ChildTransactionCreated;
+
+        /// <summary>
+        /// Returns the current <see cref="ITransaction" />.
+        /// The transaction manager will probably need to hold
+        /// the created transaction in the thread or in some sort of context.
+        /// </summary>
+        ITransaction CurrentTransaction { get; }
+
+        /// <summary>
+        /// <see cref="CreateTransaction(TransactionMode,IsolationMode,bool,bool)" />.
+        /// </summary>
+        ITransaction CreateTransaction(TransactionMode transactionMode,
+                                       IsolationMode isolationMode);
 
         /// <summary>
         /// Creates a transaction.
         /// </summary>
         /// <param name="transactionMode">The transaction mode.</param>
         /// <param name="isolationMode">The isolation mode.</param>
-        /// <param name="isAmbient">if set to <c>true</c>, the TM will create a distributed transaction.</param>
-        /// <param name="isReadOnly">if set to <c>true</c>, the TM will create a read only transaction.</param>
+        /// <param name="isAmbient">if set to <see langword="true" />, the TM will create a distributed transaction.</param>
+        /// <param name="isReadOnly">if set to <see langword="true" />, the TM will create a read only transaction.</param>
         /// <returns>
-        /// null &lt;- If transactions are just supported, but there is no ambient transaction
-        /// null &lt;- If transactions are not supported and there indeed is no ambient transaction (if there is, see exception docs)
-        ///
+        /// If transactions are just supported, but there is no ambient transaction.
+        /// If transactions are not supported and there indeed is no ambient transaction (if there is, see exception docs).
         /// </returns>
         /// <exception cref="TransactionModeUnsupportedException">
-        /// transactionMode = <see cref="TransactionMode.NotSupported"/>
-        /// and yet there is an ambient transaction in the transaction manager
-        /// which is active.
+        /// <paramref name="transactionMode" /> = <see cref="TransactionMode.NotSupported" />
+        /// and yet there is an ambient transaction in the transaction manager which is active.
         /// </exception>
-        ITransaction CreateTransaction(TransactionMode transactionMode, IsolationMode isolationMode, bool isAmbient, bool isReadOnly);
-
-        /// <summary>
-        /// Returns the current <see cref="ITransaction"/>.
-        /// The transaction manager will probably need to
-        /// hold the created transaction in the thread or in
-        /// some sort of context.
-        /// </summary>
-        ITransaction CurrentTransaction { get; }
-
-        /// <summary>
-        /// Dispose the transaction passed appropriately, removing it from the list of tracked
-        /// transactions, calling its dispose method and raise the <see cref="ITransactionManager.TransactionDisposed"/>
-        /// event.
-        /// </summary>
-        /// <param name="transaction">The transaction to dispose</param>
-        /// <exception cref="ArgumentNullException">transaction is null</exception>
-        void Dispose(ITransaction transaction);
+        ITransaction CreateTransaction(TransactionMode transactionMode,
+                                       IsolationMode isolationMode,
+                                       bool isAmbient,
+                                       bool isReadOnly);
     }
 }
