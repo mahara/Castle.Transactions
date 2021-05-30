a88dbd1d3b4b99234cb679747a989908456db715
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2021-05-30 23:19:22 +0700

    Refactored code (part b).

--- src/Castle.Services.Transaction/IsolationMode.cs
+++ src/Castle.Services.Transaction/IsolationMode.cs
@@ -17,8 +17,9 @@
 namespace Castle.Services.Transaction
 {
     /// <summary>
-    /// Isolation modes which the transactions can run in. These do not
-    /// apply equally to FtX which run as ReadCommitted for all transactions.
+    /// Isolation modes which the transactions can run in.
+    /// These do not apply equally to FileTransaction,
+    /// which run as <see cref="ReadCommitted" /> for all transactions.
     /// </summary>
     public enum IsolationMode
     {
