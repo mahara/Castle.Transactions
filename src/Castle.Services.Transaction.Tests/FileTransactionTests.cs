6b97d5cf70e5d75da168d8e553125a24592e3b04
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-11-30 15:48:31 +0700

    Refactored code (collection expressions).

--- src/Castle.Services.Transaction.Tests/FileTransactionTests.cs
+++ src/Castle.Services.Transaction.Tests/FileTransactionTests.cs
@@ -45,7 +45,7 @@ namespace Castle.Services.Transaction.Tests
 
         private readonly object _lock = new();
 
-        private readonly List<string> _pathsCreated = new();
+        private readonly List<string> _pathsCreated = [];
 
         private string _testFixtureRootDirectoryPath;
         private string _testFixtureDirectoryPath;
@@ -329,7 +329,7 @@ namespace Castle.Services.Transaction.Tests
 
                     t1_started.WaitOne();
 
-                    fs.Write(new byte[] { 0x1 }, 0, 1);
+                    fs.Write([0x1], 0, 1);
 
                     fs.Close();
                 }
