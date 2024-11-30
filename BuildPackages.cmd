eb3f34b41a589f046bb0475b4089098cb0be8bab
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-11-30 19:08:36 +0700

    Updated configuration.

--- BuildPackages.cmd
+++ BuildPackages.cmd
@@ -6,4 +6,4 @@
 IF NOT DEFINED BUILD_CONFIGURATION_FOLDER_PATH EXIT /B 1
 
 
-@CALL "%BUILD_CONFIGURATION_FOLDER_PATH%\BuildPackages.cmd" --configuration Release --version 5.4.0 %1
+@CALL "%BUILD_CONFIGURATION_FOLDER_PATH%\BuildPackages.cmd" --configuration Release --version 5.5.0 %1
