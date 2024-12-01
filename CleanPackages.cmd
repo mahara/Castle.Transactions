0bb495a933c8a5037edeb937f30379be1fa5f4b0
Maximilian Haru Raditya <mahara@users.noreply.github.com>
2024-12-01 09:01:58 +0700

    Upgraded to .NET 9.0.

--- CleanPackages.cmd
+++ CleanPackages.cmd
@@ -6,8 +6,8 @@
 IF NOT DEFINED ARTIFACTS_FOLDER_PATH EXIT /B 1
 
 
-REM dotnet clean "Castle.Transactions.sln" --configuration Debug
-REM dotnet clean "Castle.Transactions.sln" --configuration Release
+REM dotnet clean "Castle.Transactions.slnx" --configuration Debug
+REM dotnet clean "Castle.Transactions.slnx" --configuration Release
 
 IF EXIST "%ARTIFACTS_FOLDER_PATH%" (
     ECHO Deleting "%ARTIFACTS_FOLDER_PATH%" folder...
